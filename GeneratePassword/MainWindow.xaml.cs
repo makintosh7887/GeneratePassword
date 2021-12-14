7a49abf7198924fcaf1fe0020c9586017a13da5c
makintosh7887 <93168997+makintosh7887@users.noreply.github.com>
2021-12-14 04:19:08 +0300

    Добавьте файлы проекта.

---
+++ GeneratePassword/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GeneratePassword
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public string GetPass()
+        {
+            Random rnd = new Random();
+            string[] array = new string[] { "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")" };
+            string result = "";
+            for (int i = 0; i < 10; i++)
+            { 
+                result += array[rnd.Next(0,57)];
+            }
+            return result;
+        }
+
+        private void btnGenPassword_Click(object sender, RoutedEventArgs e)
+        {
+            tbPass.Text = GetPass();
+        }
+    }
+}
